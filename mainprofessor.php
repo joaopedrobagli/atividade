65247bdfbe688c830301be6f71d7a887902fe7d5
joaopedrobagli <102158248+joaopedrobagli@users.noreply.github.com>
2023-05-08 11:35:34 -0300

    Add files via upload

---
+++ mainprofessor.php
@@ -0,0 +1,17 @@
+<?php
+
+require "professor.php";
+
+//Instaciando objeto produto 1
+
+$professor1 = new professor ("Pedro","professor", "15/06/1999", "pedro.professor@gmail.com");
+
+
+
+
+$professor1->dados();
+
+
+
+
+?>
